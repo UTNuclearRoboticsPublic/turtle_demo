485e54b90202867d4491811834b55c12c2e7e690
Brandon Sheneman <sheneman23@utexas.edu>
2025-03-24 15:19:49 -0500

    Create turtle demo workspace, implement turtle chase demo from ROS2 tutorials

---
+++ turtle_ws/src/turtle_demo/launch/turtle_demo_launch.py
@@ -0,0 +1,34 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='turtlesim',
+            executable='turtlesim_node',
+            output='screen'),
+        Node(
+            package='turtle_demo',
+            executable='turtle_broadcaster',
+            name='broadcaster1',
+            parameters=[
+                {'turtlename' : 'turtle1'}
+            ]
+        ),
+        Node(
+            package='turtle_demo',
+            executable='turtle_broadcaster',
+            name='broadcaster2',
+            parameters=[
+                {'turtlename' : 'turtle2'}
+            ]
+        ),
+        Node(
+            package='turtle_demo',
+            executable='turtle_listener',
+            name='listener',
+            parameters=[
+                {'target_frame' : 'turtle1'}
+            ]
+        )
+    ])
