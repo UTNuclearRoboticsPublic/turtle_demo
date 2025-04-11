9ef676d1680f42cb27bfee5b246f4a24fa436a6a
Brandon Sheneman <sheneman23@utexas.edu>
2025-04-11 14:05:25 -0500

    Copied dynamic_selector_ros2 package from nrg_behaviors branch

---
+++ turtle_ws/src/dynamic_selector_ros2/include/dynamic_selector_ros2/decision_module.h
@@ -0,0 +1,30 @@
+#include <vector>
+#include <stdexcept>
+
+#ifndef DECISION_MODULE_H
+#define DECISION_MODULE_H
+
+using std::size_t;
+
+/**
+ * @brief The DecisionModule receives data from the DynamicSelector and returns utility values
+ * corresponding to the Selector's children.
+ * 
+ * This class is abstract. To use it, create a derived class that overrides the pure
+ * virtual method computeUtilities() with a function that returns a vector of utility values.
+ */
+class DecisionModule {
+    public:
+        DecisionModule(size_t input_size, size_t output_size);
+        virtual ~DecisionModule() = default;
+        const std::vector<float> getUtilities(const std::vector<float> input_data) const;
+
+    protected:
+        size_t input_size_;
+        size_t output_size_;
+
+    private:
+        virtual const std::vector<float> computeUtilities(const std::vector<float> input_data) const = 0;
+};
+
+#endif
---
+++ turtle_ws/src/dynamic_selector_ros2/include/dynamic_selector_ros2/smart_input_node.h
@@ -0,0 +1,49 @@
+/*  Copyright (C) 2018-2020 Davide Faconti, Eurecat -  All Rights Reserved
+*
+*   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+*   to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+*   and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+*   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*
+*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+*   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#ifndef ACTION_SMARTINPUT_NODE_H
+#define ACTION_SMARTINPUT_NODE_H
+
+#include "behaviortree_cpp/action_node.h"
+
+namespace BT
+{
+/**
+ * @brief The SmartInputNode collects data and packages it for the dynamic selector.
+ * 
+ * This class is abstract. To use it, create a derived class that overrides the pure
+ * virtual method getInputData() with a function that returns a vector of data values.
+ */
+class SmartInputNode : public SyncActionNode
+{
+public:
+  SmartInputNode(const std::string& name, const NodeConfig& config);
+  virtual ~SmartInputNode() = default;
+
+  // static PortsList providedPorts()
+  // {
+  //   PortsList ports = T::inputPorts();
+  //   ports.insert(
+  //     OutputPort<std::vector<float>>("data", "Data to send to dynamic selector")
+  //   );
+  //   return ports;
+  // }
+
+  // virtual PortsList inputPorts() = 0;
+
+private:
+  BT::NodeStatus tick() final;
+  virtual std::vector<float> getInputData() = 0;
+};
+}  // namespace BT
+
+#endif
