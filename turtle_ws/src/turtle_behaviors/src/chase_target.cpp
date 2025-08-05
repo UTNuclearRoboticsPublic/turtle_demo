3c75e96d48982ac7ba06eedf81608d4d5033c4f3
Brandon Sheneman <sheneman23@utexas.edu>
2025-08-04 20:48:16 -0500

    Made chase target more fair, added stop when target caught

--- turtle_ws/src/turtle_behaviors/src/chase_target.cpp
+++ turtle_ws/src/turtle_behaviors/src/chase_target.cpp
@@ -13,24 +13,20 @@ NodeStatus ChaseTarget::onStart() {
 }
 
 NodeStatus ChaseTarget::onRunning() {
-    static const double scaleRotationRate = 1.25;
-    static const double scaleForwardSpeed = 1.0;
-    static const double max_speed = 1.25;
-    static const double dist_threshold = 0.5;
+    static const double scaleRotationRate = 2.0;
+    static const double scaleForwardSpeed = 2.0;
+    static const double dist_threshold = 0.3;
+    static const double angle_threshold = M_PI / 8;
 
     geometry_msgs::msg::PoseStamped::SharedPtr relative_pose;
     getInput("relative_pose", relative_pose);
+
+    geometry_msgs::msg::Twist chase_velocity;
 
     double target_dist = sqrt(
         pow(relative_pose->pose.position.x, 2) +
         pow(relative_pose->pose.position.y, 2)
     );
-    double target_angle = atan2(
-        relative_pose->pose.position.y,
-        relative_pose->pose.position.x
-    );
-
-    geometry_msgs::msg::Twist chase_velocity;
 
     if (target_dist <= dist_threshold) {
         // Return 0 velocity if successful
@@ -38,9 +34,18 @@ NodeStatus ChaseTarget::onRunning() {
         setOutput("chase_velocity", chase_velocity);
         return NodeStatus::SUCCESS;
     }
+
+    double target_angle = atan2(
+            relative_pose->pose.position.y,
+            relative_pose->pose.position.x
+        );
     
-    chase_velocity.linear.x = std::max(scaleForwardSpeed * target_dist, max_speed);
-    chase_velocity.angular.z = scaleRotationRate * target_angle;
+    if (fabs(target_angle) > angle_threshold)
+        chase_velocity.angular.z = scaleRotationRate * target_angle / fabs(target_angle);
+    else
+        chase_velocity.linear.x = scaleForwardSpeed;
+    
+    
     setOutput("chase_velocity", chase_velocity);
     return NodeStatus::RUNNING;
 }
