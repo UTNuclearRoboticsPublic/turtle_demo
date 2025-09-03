29d5eaea918f4261a7cf495b8a016fad246cee27
Brandon Sheneman <sheneman23@utexas.edu>
2025-09-02 20:50:23 -0500

    Monitor records average distance between turtles

--- turtle_ws/src/turtle_demo/src/turtle_monitor.cpp
+++ turtle_ws/src/turtle_demo/src/turtle_monitor.cpp
@@ -1,15 +1,28 @@
-#include <rclcpp/rclcpp.hpp>
 #include <fstream>
+#include <rclcpp/rclcpp.hpp>
+#include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
 #include <std_srvs/srv/trigger.hpp>
 
 class TurtleMonitor : public rclcpp::Node {
 public:
     TurtleMonitor(): Node("turtle_monitor") {
+        // Parameters
+        csv_path_ = this->declare_parameter("csv_path", "/tmp/turtle.csv");
+
+        // Services
         start_monitor_ = this->create_service<std_srvs::srv::Trigger>("start_monitor",
             std::bind(&TurtleMonitor::startMonitorCallback, this, std::placeholders::_1, std::placeholders::_2));
         end_monitor_ = this->create_service<std_srvs::srv::Trigger>("end_monitor",
             std::bind(&TurtleMonitor::endMonitorCallback, this, std::placeholders::_1, std::placeholders::_2));
-        csv_path_ = this->declare_parameter("csv_path", "/tmp/turtle.csv");
+
+        // Timer
+        timer_ = this->create_wall_timer(std::chrono::duration<double>(0.01), std::bind(&TurtleMonitor::timerCallback, this));
+
+        // TF listener
+        tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+        tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
+
         RCLCPP_INFO(this->get_logger(), "Monitor ready.");
     }
 
@@ -32,16 +45,41 @@ private:
         auto start_ctime = std::string(std::ctime(&start_time_t));
         start_ctime.erase(std::remove(start_ctime.begin(), start_ctime.end(), '\n'), start_ctime.end());
 
+        // Get average distance between turtles
+        float avg_dist = total_distance_ / dist_count_;
+
         // Write info to CSV file
         std::ofstream csv(csv_path_, std::ios::app);
-        csv << start_ctime << ',' << time_span.count() << '\n';
+        csv << start_ctime << ',' << time_span.count() << ',' << avg_dist << '\n';
         csv.close();
 
         resp->success = true;
     }
 
+    void timerCallback() {
+        // Wait for turtles to start moving
+        // Get distance between turtles
+        if (tf_buffer_->canTransform("turtle1", "turtle2", tf2::TimePointZero)) {
+            const geometry_msgs::msg::TransformStamped tf = tf_buffer_->lookupTransform(
+                "turtle1", "turtle2", tf2::TimePointZero
+            );
+
+            // Add distance to the total
+            float dist = sqrt(pow(tf.transform.translation.x, 2) + pow(tf.transform.translation.y, 2));
+            total_distance_ += dist;
+
+            // Increment distance count by 1
+            dist_count_++;
+        }
+    }
+
     rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr start_monitor_{nullptr};
     rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr end_monitor_{nullptr};
+    rclcpp::TimerBase::SharedPtr timer_{nullptr};
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_{nullptr};
+    std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
+    float total_distance_ = 0;
+    size_t dist_count_ = 0;
     std::chrono::time_point<std::chrono::system_clock> start_time_;
     std::string csv_path_;
 };
