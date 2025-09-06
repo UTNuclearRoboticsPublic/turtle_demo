7216f0e5e268441ccf7a812939a7f65022f1970d
Brandon Sheneman <sheneman23@utexas.edu>
2025-09-06 14:18:09 -0500

    Added autopilot control launch

---
+++ turtle_ws/src/turtle_demo/launch/turtle_demo_auto_control_launch.py
@@ -0,0 +1,61 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='turtlesim_ds',
+            executable='turtlesim_node',
+            output='screen'
+        ),
+        Node(
+            package='turtle_demo',
+            executable='turtle_broadcaster',
+            name='broadcaster1',
+            parameters=[
+                {'turtlename' : 'turtle1'}
+            ]
+        ),
+        Node(
+            package='turtle_demo',
+            executable='turtle_broadcaster',
+            name='broadcaster2',
+            parameters=[
+                {'turtlename' : 'turtle2'}
+            ]
+        ),
+        Node(
+            package='turtle_demo',
+            executable='turtle_listener',
+            name='listener',
+            parameters=[
+                {'target_frame' : 'turtle1'}
+            ]
+        ),
+        Node(
+            package='turtle_demo',
+            executable='turtle_BT',
+            name='turtle_BT',
+            output='screen',
+            arguments=['/home/sheneman/thesis/turtle_ws/src/turtle_demo/behavior_trees/control_tree.xml'],
+            ros_arguments=['--log-level', 'warn']
+        ),
+        Node(
+            package='turtle_demo',
+            executable='turtle_service_handler',
+            name='turtle_service_handler'
+        ),
+        Node(
+            package='turtle_demo',
+            executable='turtle_monitor',
+            name='turtle_monitor',
+            parameters=[
+                {'csv_path' : '/home/sheneman/thesis/turtle_auto_control.csv'}
+            ]
+        ),
+        Node(
+            package='turtle_demo',
+            executable='turtle_autopilot',
+            name='turtle_autopilot'
+        )
+    ])
